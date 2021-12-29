b3710655e3dcbf97163280d5e8cfe2eb6ed17dcf
cassemanuel <cassioefs@dcc.ufrj.br>
2021-12-29 02:16:31 -0300

    Movimento especial: Promoção do peão

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,13 @@ public class Program {
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
+
+					if (chessMatch.getPromoted() != null) {
+						System.out.print("Enter piece for promotion (B/N/R/Q): ");
+						String type = sc.nextLine();
+						chessMatch.replacePromotedPiece(type);
+					}
+
 				}
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
